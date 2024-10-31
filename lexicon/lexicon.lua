0bbd83bbdb7e8229bc1fa0a90d4a5cbb4c3f0bd6
alexfayers <alex@fayers.com>
2024-10-31 15:59:22 +0000

    lexicon working init

--- lexicon/lexicon.lua
+++ lexicon/lexicon.lua
@@ -1,12 +1,23 @@
 -- The main lexicon program.
 -- From here, you can access all of lexicon's subprograms!
 
-local MANIFEST_URL = "https://raw.githubusercontent.com/alexfayers/lexicon/main/lexicon/lexicon-db.json"
+local MANIFEST_URL = "https://raw.githubusercontent.com/alexfayers/cc-24/main/lexicon/lexicon-db.json"
+
+-- local pretty = require("cc.pretty")
+
+---Add a token to the end of a URL to prevent caching
+---@param url string
+---@return string
+local function addTokenToUrl(url)
+    return url .. "?token=" .. os.epoch("utc")
+end
 
 ---Get the latest manifest from the lexicon repository
 ---@return table
 local function getLatestManifest()
-    local request = http.get(MANIFEST_URL)
+    -- use a token to prevent caching
+    ---@diagnostic disable-next-line: undefined-field
+    local request = http.get(addTokenToUrl(MANIFEST_URL))
     if request then
         local manifest = textutils.unserialiseJSON(request.readAll())
         request.close()
@@ -16,24 +27,35 @@ local function getLatestManifest()
     error("Failed to get latest manifest")
 end
 
+local manifest = getLatestManifest()
+
 
 ---Download a package from the lexicon repository
 ---@param packageName string The name of the package to download
-local function downloadPackage(packageName)
-    local manifest = getLatestManifest()
-    local packageData = manifest[packageName]
+---@param parentPackage string | nil The name of the parent package
+local function downloadPackage(packageName, parentPackage)
+    -- pretty.print(pretty.pretty(manifest))
+    local packageData = manifest["packages"][packageName]
 
     if not packageData then
-        error("Package not found")
+        error("Package '" .. packageName .. "' not found")
     end
 
-    print("Downloading " .. packageName .. " (version " .. packageData["version"] .. ")...")
+    local downloadMessage = "Downloading " .. packageName .. " (" .. packageData["version"] .. ")"
+    if parentPackage then
+        term.setTextColor(colors.gray)
+        downloadMessage = downloadMessage .. " for " .. parentPackage
+    end
+    print(downloadMessage .. "...")
+    term.setTextColor(colors.white)
 
     local packageDependencies = packageData["dependencies"]
-    if packageDependencies then
-        print("Found " .. #packageDependencies .. " dependencies, downloading them first...")
-        for dependencyName, _ in pairs(packageDependencies) do
-            downloadPackage(dependencyName)
+    if #packageDependencies > 0 then
+        -- term.setTextColor(colors.blue)
+        -- print("Found " .. #packageDependencies .. " dependencies for '" .. packageName .. "'")
+        -- term.setTextColor(colors.white)
+        for _, dependecyName in pairs(packageDependencies) do
+            downloadPackage(dependecyName, packageName)
         end
     end
 
@@ -43,7 +65,7 @@ local function downloadPackage(packageName)
         local sourceUrl = file[1]
         local downloadPath = file[2]
 
-        local request = http.get(sourceUrl)
+        local request = http.get(addTokenToUrl(sourceUrl))
         if request then
             local fileContents = request.readAll()
             request.close()
@@ -59,9 +81,26 @@ local function downloadPackage(packageName)
             error("Failed to download file")
         end
     end
+
+    if not parentPackage then
+        term.setTextColor(colors.lime)
+        print("Downloaded " .. packageName .. " (" .. packageData["version"] .. ")")
+        term.setTextColor(colors.white)
+    end
+
+    -- Check if the program path is set
+    if packageData["program-path"] then
+        local globalPath = shell.path()
+        local path = packageData["program-path"]
+        if not globalPath:find(path, 1, true) then
+            shell.setPath(globalPath .. ":" .. path)
+        end
+        -- term.setTextColor(colors.grey)
+        -- print("Added " .. packageName .. " to the shell path")
+        -- term.setTextColor(colors.white)
+    end
 end
 
 
 -- Download package-example as a test
-print("Downloading package-example!")
-downloadPackage("package-example")
+downloadPackage("package-example", nil)
