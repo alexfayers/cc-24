0e93ff7b51620cf00d103382e7b14515ac791123
alexfayers <alex@fayers.com>
2024-10-31 16:42:05 +0000

    add logging examples to example package

--- packages/package-example/package-example.lua
+++ packages/package-example/package-example.lua
@@ -1,6 +1,7 @@
 -- Example package that uses lexicon-lib
 package.path = package.path .. ";/usr/lib/?.lua"
 
+-- Terminal example
 local terminal = require("lexicon-lib.lib-term")
 
 terminal.writeWrap("Hello from package-example!")
@@ -10,4 +11,10 @@ terminal.newline()
 terminal.writeWrapColor("It can output in color too!", colors.lime)
 terminal.newline()
 
+-- Logging example
 local logging = require("lexicon-lib.lib-logging")
+local logger = logging.getLogger("package-example")
+
+logger:info("This is an info message")
+logger:warn("This is a warning message")
+logger:error("This is an error message")
