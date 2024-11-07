9736b33ece3a72436f34db0d40fcfc6fab9780d1
alexfayers <alex@fayers.com>
2024-11-07 22:51:40 +0000

    remove busy command

--- packages/library/lib-storage2/remote/enums.lua
+++ packages/library/lib-storage2/remote/enums.lua
@@ -3,11 +3,9 @@
 ---@enum MessageType
 MessageType = {  ---@class _MessageType
     ACKNOWLEDGE = "ACK",
-    BUSY = "BUSY",
     INVALID_DATA_TYPE = "INVALID_DATA_TYPE",
     UNKNOWN_COMMAND = "UNKNOWN_COMMAND",
     DONE = "DONE",
-
     UNKNOWN_ERROR = "UNKNOWN_ERROR",
 
     COMMAND_REFRESH = "COMMAND_REFRESH",
