a5295ac817074886e789c9da48b4f15e06a3d18a
alexfayers <alex@fayers.com>
2024-12-17 22:19:18 +0000

    fix: refine Turtle initialization by adjusting position assignment logic

--- packages/library/lib-turtle/Turtle.lua
+++ packages/library/lib-turtle/Turtle.lua
@@ -56,8 +56,10 @@ function Turtle:init(startingPosition)
 
     self:loadState()
 
-    self.position = self.position or Turtle.origin
-    self.startingPosition = startingPosition or self.startingPosition or self.position
+    self.startingPosition = startingPosition or self.startingPosition or Turtle.origin
+    self.position = self.position or self.startingPosition
+
+    self.resumePosition = self.resumePosition or nil
 
     ---@type TurtleInventory
     self.inventory = TurtleInventory()
@@ -88,8 +90,6 @@ function Turtle:init(startingPosition)
     if not string.match(self.currentlyEquipped.name, "pickaxe") then
         self:equipPickaxe()
     end
-
-    self.resumePosition = self.resumePosition or nil
 
     self:saveState()
 end
